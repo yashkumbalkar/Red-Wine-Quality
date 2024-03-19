058737e50bed46da72ca7bdea97e05a22ea11fe7
yash kumbalkar <yashkumbalkar@gmail.com>
2024-03-19 15:03:48 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from src.Red_Wine_Quality import logger
+from Red_Wine_Quality import logger
+from Red_Wine_Quality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info('This is our custom log!')
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
 
