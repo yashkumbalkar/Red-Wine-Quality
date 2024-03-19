6f8b502cb1e45a8215965d5c96f66b7388231c49
yash kumbalkar <yashkumbalkar@gmail.com>
2024-03-19 23:40:04 +0530

    model evaluation  added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from Red_Wine_Quality.pipeline.stage_01_data_ingestion import DataIngestionTrain
 from Red_Wine_Quality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from Red_Wine_Quality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from Red_Wine_Quality.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from Red_Wine_Quality.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
 
 
 
@@ -48,6 +50,17 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
